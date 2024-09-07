d8af55e98093bb149b0fa3dd5509339a92f6708f
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-09-06 22:18:50 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,31 @@
-"""
-This module will enable customers to access this application on the web.
-"""
-
-from flask import Flask, render_template, request
-from EmotionDetection import emotion_detection
+rom flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emo_detector():
-    """
-    This function will take a text input from a user interface and
-    pass back the emotion score of that text input.
-    """
-    text_to_analyze = request.args.get('textToAnalyze')
-    dominant_emotion = emotion_detection.emotion_detector(text_to_analyze)
-    if dominant_emotion is None:
-        return "Invalid text! Please try again!"
-    
-    anger = dominant_emotion['anger']
-    disgust = dominant_emotion['disgust']
-    fear = dominant_emotion['fear']
-    joy = dominant_emotion['joy']
-    sad = dominant_emotion['sadness']
-    max_emotion = max(dominant_emotion.items(), key=lambda x: x[1])[0]
-    
-    return (f"For the given statement, the system response is "
-            f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, "
-            f"'sadness': {sad}. "
-            f"The dominant emotion is {max_emotion}")
+def emotion_detector_function():
+    ''' This function calls the application
+    '''
+    test_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
 
-@app.route("/")
+    if response['dominant_emotion'] is None:
+        response_text = "Invalid Input! Please try again."
+    else:
+        response_text = f"For the given statement, the system response is 'anger': \
+                    {response['anger']}, 'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, 'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. The dominant emotion is \
+                    {response['dominant_emotion']}."
+
+    return response_text
+
+@app.route("\")
 def render_index_page():
-    """
-    This function will render the user interface
-    """
+    ''' This is the function to render the html interface
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host = "0.0.0.0", port = 5000)
