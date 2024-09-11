429d8b79bbd8add0d020ac8124fde68616616326
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-09-11 01:28:21 -0400

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,17 +1,22 @@
 import unittest
-from EmotionDetection.emotion_detection import emotion.detector
- 
+from EmotionDetection.emotion_detection import emotion_detector
+
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
-        result1 = emotion.detector(“I am glad this happened”)
-        self.assertEqual(result1[‘dominant_emotion’], ’joy’)
-        result2 = emotion.detector(“I am really mad about this”)
-        self.assertEqual(result2[‘dominant_emotion’], ‘anger’)
-        result3 = emotion.detector(“I feel disgusted just hearing about this”)
-        self.assertEqual(result3[‘dominant_emotion’], ’ disgust’)
-        result4 = emotion.detector(“I am so sad about this”)
-        self.assertEqual(result4[‘dominant_emotion’], ’sadness’)
-        result5 = emotion.detector(“I am really afraid that this will happen”)
-        self.assertEqual(result5[‘dominant_emotion’], ’fear’)
+        result_1 = emotion_detector('I am glad this happened')
+        self.assertEqual(result_1, 'joy')
 
-unittest.main()
+        result_2 = emotion_detector('I am really mad about this')
+        self.assertEqual(result_2, 'anger')
+
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(result_3, 'disgust')
+
+        result_4 = emotion_detector('I am so sad about this')
+        self.assertEqual(result_4, 'sadness')
+
+        result_5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result_5, 'fear')
+
+if __name__ == '__main__':
+    unittest.main()
