36d5515c17a2bb278fc39d8ad1891c2b0db60707
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-09-11 02:02:33 -0400

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,25 +1,34 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyse):
+def emotion_detector(text_to_analyze):
+    if not text_to_analyze.strip():
+        return None
 
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = {"raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = myobj, headers = headers)
-    status_code = response.status_code
-    
-    if status_code == 400:
-        formatted_output = { 'anger': None,
-                             'disgust': None,
-                             'fear': None,
-                             'joy': None,
-                             'sadness': None,
-                             'dominant_emotion': None }
-    else:
-        res = json.loads(response.text)
-        formatted_output = res[‘emotionPredictions’][0][‘emotion’]
-        dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
-        formatted_response[‘dominant_dictionary’] = dominant_emotion
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json=input_json, headers=header)
+    formatted_response = json.loads(response.text)
 
-    return formatted_response 
+    if response.status_code == 200:
+        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
+
+        return {
+            'anger': emotion_predictions['anger'],
+            'disgust': emotion_predictions['disgust'],
+            'fear': emotion_predictions['fear'],
+            'joy': emotion_predictions['joy'],
+            'sadness': emotion_predictions['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
